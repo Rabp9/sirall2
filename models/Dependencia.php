f7fdca277e8e0f01f07252cffc170c29aeb92f55
essalud <essalud@essaludrall>
2013-08-15 09:34:17 -0500

    aplicando aun mas el MVC

--- models/Dependencia.php
+++ models/Dependencia.php
@@ -1,13 +1,15 @@
 <?php
+    require '../Libs/BaseDatos.php';
+    
     class Dependencia {
         private $idDependencia;
         private $descripcion;
-        private $superIdDependencia;
+        private $superDependencia;
         
-        public function __construct() {
+        /*public function __construct() {
             $this->idDependencia = 0;
             $this->descripcion = "";
-        }
+        }*/
 
         //Sets
         public function setIdDependencia($idDependencia) {
@@ -18,8 +20,8 @@
             $this->descripcion = $descripcion;
         }
         
-        public function setSuperIdDependencia(Dependencia $dependencia) {
-            $this->descripcion = $dependencia;
+        public function setSuperDependencia(Dependencia $dependencia) {
+            $this->superDependencia = $dependencia;
         }
 
         //Gets
@@ -31,8 +33,35 @@
             return $this->descripcion;
         }
         
-        public function getSuperIdDependencia() {
-            return $this->superIdDependencia;
+        public function getSuperDependencia() {
+            return $this->superDependencia;
+        }
+        
+        public static function getDependenciaByIdDependencia($idDependencia) {
+            $result = BaseDatos::getDbh()->prepare("SELECT * FROM Dependencia where idDependencia = :idDependencia");
+            $result->bindParam(':idDependencia', $idDependencia);
+            $result->execute();
+            $dependencia = new Dependencia();
+            if($rs = $result->fetch()) {
+                $dependencia->setIdDependencia($rs['idDependencia']);
+                $dependencia->setDescripcion($rs['descripcion']);
+                if($rs['superIdDependencia'] != NULL)
+                    $dependencia->setSuperDependencia (self::getDependenciaByIdDependencia($rs['superIdDependencia']));
+            }
+            else return false;
+            return $dependencia;
+        }
+        
+        public static function getAllDependencia() {
+            $result = BaseDatos::getDbh()->query("SELECT * FROM Dependencia");
+            $dependencia = new Dependencia();
+            while ($rs = $result->fetch()) {
+                $dependencia->setIdDependencia($rs['idDependencia']);
+                $dependencia->setDescripcion($rs['descripcion']);
+                //$dependencia->setSuperDependencia($rs['superIdDependencia']);
+                $dependencias[] = $dependencia; 
+            }
+            return $dependencias;
         }
     }
 ?>
---
+++ models/prueba.php
@@ -0,0 +1,3 @@
+<?php
+    require_once './Dependencia.php';
+?>
