15199c0f37106327ac8e01f2c993ba450b4e00da
essalud <essalud@essaludrall>
2013-11-20 16:45:56 -0500

    tecnico

--- DAO/AppDAO.php
+++ DAO/AppDAO.php
@@ -1,9 +1,8 @@
 <?php
-    interface appDAO {
+    interface AppDAO {
         function getAll();
         function crear($object);
         function editar($object);
         function eliminar($object);
         function getBy($campo, $valor);
-        function getBy($campos = array(), $valores = array());
 }
--- models/VwUsuario.php
+++ models/VwUsuario.php
@@ -1,7 +1,8 @@
 <?php
-
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+    /*
+     * Clase Vista de Usuario
+     */
+    class VwUsuario {
+        
+    }
 ?>
